b4a178376f42a8d56f666fc6f2f5ef3969d3a2d7
Jayden Ly <jtl5627@utulsa.edu>
2024-02-22 17:16:25 -0600

    4

--- Problem 4/reverse_string.py
+++ Problem 4/reverse_string.py
@@ -8,10 +8,12 @@ Lab Time: thursday 2:52
 def reverse_string():
     while True:
         og_string = input()
-        if og_string.lower() in ['d']:
+        if og_string.lower() in ['d','done']:
             break
 
         reversed_string = og_string[::-1]
         print(reversed_string)
+    
+    pass
 if __name__ == "__main__":
     reverse_string()
